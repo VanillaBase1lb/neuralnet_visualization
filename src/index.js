8e3b2b91110244e960433040396d23031544c7be
Yuvraj Mishra <40194399+VanillaBase1lb@users.noreply.github.com>
2022-10-25 13:08:19 +0530

    fix view

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import * as d3 from "d3";
 
-const hidden_layer_count = 2;
+const hidden_layer_count = 3;
 let node_count = new Array(hidden_layer_count + 2).fill(0);
 node_count[0] = 3;
 node_count[1] = 2;
-node_count[2] = 9;
-node_count[3] = 1;
+node_count[2] = 2;
+node_count[3] = 9;
+node_count[4] = 1;
 // console.log(node_count);
 
-const boxHeight = 300;
-const width = 300;
+const boxHeight = 600;
+const width = 600;
 const svg = d3.create("svg").attr("viewBox", [0, 0, width, boxHeight]);
 let nodes = [];
 // nodes.push([width / 2, boxHeight / 1.5]);
@@ -18,7 +19,7 @@ let nodes = [];
 for (let i = 0; i < hidden_layer_count + 2; i++) {
   for (let j = 0; j < node_count[i]; j++) {
     nodes.push([
-      (width / (hidden_layer_count + 2)) * (i + 1),
+      (width / (hidden_layer_count + 2)) * (i + 0.5),
       (boxHeight / (node_count[i] + 1)) * (j + 1),
     ]);
   }
@@ -30,7 +31,7 @@ for (let i = 0; i < nodes.length; i++) {
     .attr("cx", nodes[i][0])
     .attr("cy", nodes[i][1])
     .attr("r", 10)
-    .style("fill", "green");
+    .style("fill", "red");
 }
 
 // let links = [];
