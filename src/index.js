f7dbfff6ec0725b8de923cefa7fc29f857c70e89
Yuvraj Mishra <40194399+VanillaBase1lb@users.noreply.github.com>
2022-11-21 12:35:52 +0530

    add bias neuron

--- src/index.js
+++ src/index.js
@@ -10,12 +10,12 @@ function initZoom() {
   d3.select("svg").call(zoom);
 }
 
-const hidden_layer_count = 3;
-let node_count = new Array(hidden_layer_count + 2).fill(0);
+const layer_count = 5;
+let node_count = new Array(layer_count).fill(0);
 node_count[0] = 3;
 node_count[1] = 2;
 node_count[2] = 2;
-node_count[3] = 9;
+node_count[3] = 6;
 node_count[4] = 1;
 // console.log(node_count);
 
@@ -24,40 +24,56 @@ const width = 600;
 const svg = d3.create("svg").attr("viewBox", [0, 0, width, boxHeight]);
 const g = svg.append("g");
 let nodes = [];
-// nodes.push([width / 2, boxHeight / 1.5]);
-// nodes.push([width / 4, boxHeight / 3]);
-// nodes.push([width / 1.5, boxHeight / 3]);
-for (let i = 0; i < hidden_layer_count + 2; i++) {
+
+// add neurons
+for (let i = 0; i < layer_count; i++) {
   nodes[i] = [];
-  for (let j = 0; j < node_count[i]; j++) {
+  let nodecount = node_count[i] + 1; // for bias neuron
+  for (let j = 0; j < nodecount; j++) {
     nodes[i].push({
-      x: (width / (hidden_layer_count + 2)) * (i + 0.5),
-      y: (boxHeight / (node_count[i] + 1)) * (j + 1),
+      x: (width / layer_count) * (i + 0.5),
+      y: (boxHeight / (nodecount + 1)) * (j + 1),
     });
   }
 }
 
-for (let i = 0; i < hidden_layer_count + 1; i++) {
-  for (let j = 0; j < node_count[i]; j++) {
-    for (let k = 0; k < node_count[i + 1]; k++) {
+// draw lines
+for (let i = 0; i < layer_count - 1; i++) {
+  for (let j = 0; j < node_count[i] + 1; j++) {
+    for (let k = 1; k < node_count[i + 1] + 1; k++) {
+      let color = "black";
+      // bias connection
+      if (j == 0) {
+        color = "gray";
+      }
       g.append("line")
         .attr("x1", nodes[i][j].x)
         .attr("y1", nodes[i][j].y)
         .attr("x2", nodes[i + 1][k].x)
         .attr("y2", nodes[i + 1][k].y)
-        .style("stroke", "black")
+        .style("stroke", color)
         .style("stroke-width", 1);
     }
   }
 }
 
-for (let i = 0; i < hidden_layer_count + 2; i++) {
-  for (let j = 0; j < node_count[i]; j++) {
+// draw neurons
+for (let i = 0; i < layer_count; i++) {
+  for (let j = 0; j < node_count[i] + 1; j++) {
+    // output layer bias
+    if (i == layer_count - 1 && j == 0) {
+      continue;
+    }
+    let color = "red";
+    // bias neuron
+    if (j == 0) {
+      color = "yellow";
+    }
     g.append("circle")
       .attr("cx", nodes[i][j].x)
       .attr("cy", nodes[i][j].y)
       .attr("r", 10)
-      .style("fill", "red");
+      .style("fill", color);
   }
 }
 
